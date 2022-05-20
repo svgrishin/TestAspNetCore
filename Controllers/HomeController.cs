eb06e4948491aba9ec03f96f2f9b8fd07e218338
lrugrs1 <dr_grishin_91@mail.ru>
2022-05-20 17:31:02 +0300

    Работают вычисления. Доделать память, сохранение, чтение сохранений

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -1,6 +1,7 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using System.Diagnostics;
 using TestAspNetCore.Models;
+using TestAspNetCore.Controllers.Models;
 
 namespace TestAspNetCore.Controllers
 {
@@ -13,8 +14,38 @@ namespace TestAspNetCore.Controllers
             _logger = logger;
         }
 
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+
+
+
+        //===============================================================================================================
+        //===============================================================================================================
+        //===============================================================================================================
+
+
+
+        private static string location = "C:/Users/Public/Documents/calc.json";
+
+        public static List<string> resultStrings = new List<string>();
+
+        /// <summary>
+        /// Объект калькулятора
+        /// </summary>
+
+        public static Calculator calc = new Calculator();
+
+        /// <summary>
+        /// Массив с калькуляторами для хранения объектов с различными состояниями
+        /// </summary>
+        public static Calculator[] calcs = new Calculator[0];
+
         public IActionResult Index()
         {
+            TempData["display"] = "0";
             return View();
         }
 
@@ -23,12 +54,250 @@ namespace TestAspNetCore.Controllers
             return View();
         }
 
-        
-
-        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
-        public IActionResult Error()
+        public IActionResult Number_Click(string btn_number)
         {
-            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+            inputVal(btn_number[0]);
+
+            return View("Index");
+        }
+
+        private void inputVal(char c)
+        {
+            TempData["display"] = calc.inputValues(c);
+        }
+
+        public IActionResult btn_Zero_Click()
+        {
+            if (calc.arg == "") typeZeroComa();
+            else TempData["display"] = calc.inputValues('0');
+
+            return View("Index");
+        }
+
+        public void typeZeroComa()
+        {
+            TempData["display"] = calc.inputValues('0');
+            TempData["display"] = calc.inputValues(',');
+        }
+
+        public IActionResult btn_CE_Click1()
+        {
+            calc.arg = "";
+            calc.disp = "0";
+            TempData["display"] = calc.disp;
+
+            return View("Index");
+        }
+
+        public IActionResult btn_C_Click()
+        {
+            calc.ResetCalc();
+            Array.Clear(calc.args, 0, 1);
+            TempData["display"] = "0";
+
+            return View("Index");
+        }
+
+        public IActionResult btn_Backspase_Click()
+        {
+            TempData["display"] = calc.deleteSymbol();
+            return View("Index"); 
+        }
+
+        public IActionResult btn_SQR_Click()
+        {
+            calc.symbol = "^";
+            calc.fDeleg = new Calculator.CalcFunction().sqrOf;
+            btn_Func_Click(calc.fDeleg, true);
+
+            return View("Index");
+        }
+
+        public IActionResult btn_SQRT_Click()
+        {
+            calc.symbol = "√";
+            calc.fDeleg = new Calculator.CalcFunction().sqrtOf;
+            btn_Func_Click(calc.fDeleg, true);
+
+            return View("Index");
+        }
+
+        public IActionResult btn_Plus_Click()
+        {
+            btn_click("+", new Calculator.CalcFunction().Summ, false);
+            return View("Index");
+        }
+
+        public IActionResult btn_Minus_Click()
+        {
+            btn_click("-", new Calculator.CalcFunction().differens, false);
+            return View("Index");
+        }
+
+        public IActionResult btn_Multiply_Click()
+        {
+            btn_click("×", new Calculator.CalcFunction().multiply, false);
+            return View("Index");
+        }
+
+        public IActionResult btn_Divide_Click()
+        {
+            btn_click("÷", new Calculator.CalcFunction().divide, false);
+
+            return View("Index");
+        }
+
+        private void btn_click(string s, Calculator.funcDeleg f, bool isExtraFunc)
+        {
+            calc.symbol = s;
+
+            if (calc.funcFlag == true)
+            {
+                calc.fDeleg = f;
+                calc.resBtnFlag = false;
+            }
+            else
+            {
+                btn_Func_Click(f, isExtraFunc);
+            }
+        }
+
+        /// <summary>
+        /// Нажатие одной из функций
+        /// </summary>
+        /// <param name = "f" > Делегат функции выполнения</param>
+        /// <param name = "isExtraFunc" > Флаг функции одного аргумента</param>
+        private void btn_Func_Click(Calculator.funcDeleg f, bool isExtraFunc)
+        {
+            if (isExtraFunc == true)
+            {
+                calc.extraFunc(f);
+                TempData["display"] = calc.disp;
+                saveStatus();
+            }
+            else funcClick(f);
+        }
+
+        private void funcClick(Calculator.funcDeleg f)
+        {
+            calc.resBtnFlag = false;
+
+            switch (calc.index)
+            {
+                case false:
+                    {
+                        calc.fDeleg = f;
+                        calc.tryToGetArg(calc.arg);
+                        calc.funcFlag = true;
+                        break;
+                    }
+                case true:
+                    {
+                        calc.tryToGetArg(calc.arg); ;
+                        getResult(calc.fDeleg);
+                        calc.funcFlag = true;
+                        calc.fDeleg = f;
+                        break;
+                    }
+            }
+
+            TempData["display"] = calc.disp;
+            calc.minus = false;
+        }
+
+        /// <summary>
+        /// Инициатор получения результата
+        /// </summary>
+        /// <param name="cf">Делегат функции</param>
+        private void getResult(Calculator.funcDeleg cf)
+        {
+            calc.getResult(cf);
+            saveStatus();
+        }
+
+
+        private void saveStatus()
+        {
+            saveMe();
+            if (calc.resultString != "") addToCalcList(calc);
+            calc.resultString = "";
+        }
+
+        /// <summary>
+        /// Добавление в список вычисления целиком
+        /// </summary>
+        /// <param name="c">Калькулятор</param>
+        private void addToCalcList(Calculator c)
+        {
+            resultStrings.Add(c.resultString);
+        }
+
+        public void saveMe()
+        {
+            addCalc();
+            try
+            {
+                SaverLoader.saveCalc(calc);
+            }
+            catch { }
+        }
+
+        private void addCalc()
+        {
+            int i = calcs.Length;
+            Array.Resize(ref calcs, i + 1);
+            calcs[calcs.Length - 1] = new Calculator(calc);
+        }
+
+        public IActionResult btn_History_Click()
+        {
+            SaverLoader.loadHistory(location, calc.resultString);
+
+            return View("Index");
+        }
+
+        public IActionResult btn_Negative_Click()
+        {
+            calc.minus = !calc.minus;
+            inputVal('-');
+
+            return View("Index");
+        }
+
+        public IActionResult btn_Coma_Click()
+        {
+            if (calc.arg.Contains(',') == false)
+            {
+                if (calc.arg == "")
+                {
+                    typeZeroComa();
+                }
+                else TempData["display"] = calc.inputValues(',');
+            }
+
+            else TempData["display"] = calc.arg;
+
+            return View("Index");
+        }
+
+        public IActionResult btn_Result_Click()
+        {
+            try
+            {
+                calc.tryToGetArg(calc.arg);
+
+                calc.resBtnFlag = true;
+                calc.funcFlag = true;
+
+                calc.getResult(calc.fDeleg);
+
+                TempData["display"] = calc.disp;
+
+                saveStatus();
+            }
+            catch { }
+
+            return View("Index");
         }
     }
 }
---
+++ Controllers/Models/SaverLoader.cs
@@ -0,0 +1,48 @@
+﻿using Newtonsoft.Json;
+
+namespace TestAspNetCore.Controllers.Models
+{
+    public abstract class SaverLoader
+    {
+        /// <summary>
+        /// Сохранение состояния калькулятора в файл
+        /// </summary>
+        public static void saveCalc(Calculator c)
+        {
+            Calculator newC = new Calculator(c);
+
+            newC.fDeleg = null;
+
+            string s = JsonConvert.SerializeObject(newC);
+
+            string location = Directory.GetCurrentDirectory() + "/calc.json";
+            File.AppendAllText(location, s + "\n");
+        }
+
+        public static void loadHistory(string location, string resultstring)
+        {
+            string[] s = File.ReadAllLines(location);
+            int i = 0;
+            foreach (string str in s)
+            {
+                Array.Resize(ref HomeController.calcs, i + 1);
+                //Array.Resize(ref HomeController.resultStrings, i + 1);
+
+                HomeController.calcs[i] = JsonConvert.DeserializeObject<Calculator>(str);
+
+                //HomeController.resultStrings[i] = HomeController.calcs[i].resultString;
+                HomeController.resultStrings.Add(HomeController.calcs[i].resultString);
+                i++;
+            }
+
+            //try
+            //{
+            //    foreach (string c in HomeController.resultStrings)
+            //    {
+            //        HomeController.calcList.Add(c);
+            //    }
+            //}
+            //catch (Exception ex) { }
+        }
+    }
+}
